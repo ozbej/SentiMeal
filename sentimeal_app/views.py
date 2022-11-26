6fe6f198c4622b891a891ea1c5c7a0f47a9c5280
Anukun Thurner <anukun.thurner@student.tugraz.at>
2022-11-26 15:10:29 +0100

    implement map

--- sentimeal_app/views.py
+++ sentimeal_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+#from .models import Items
 # Create your views here.
 def home(request):
+    #if request.method == 'POST':
+        #Items.objects.create(name = request.POST['name'])
+
+    #all_items = Items.objects.filter('restaurant name')
+
     return render(request, 'home.html')
